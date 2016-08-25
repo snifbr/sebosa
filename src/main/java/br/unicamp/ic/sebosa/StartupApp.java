10f98109cedf1a99f3bfecd6ea4b56efda172298
TurmaB - Rafael Campos Jonas <rfhjones@gmail.com>
2016-08-24 22:34:56 -0300

    Rotas updated

--- src/main/java/br/unicamp/ic/sebosa/StartupApp.java
+++ src/main/java/br/unicamp/ic/sebosa/StartupApp.java
@@ -4,6 +4,17 @@ import static spark.Spark.*;
 
 public class StartupApp {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Sebosa diz: olá!");
+    	//book
+        get("/book", (req, res) -> "Olá, eu sou um livro!");
+        post("/book/:isbn", (request, response) -> {
+        	return "Adicionar livro, isbn = " + request.params(":isbn");
+        });
+        put("/book/update/:isbn", (request, response) -> {
+        	return "Atualizar livro, isbn = " + request.params(":isbn");
+        });
+        delete("/book/delete/:isbn", (request, response) -> {
+        	return "Deletar livro, isbn = " + request.params(":isbn");
+        });
+        options("/options", (request, response) -> "Options");
     }
 }
